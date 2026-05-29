e5f82e2adaae22d71643f84a48b1029ed66ef7ea
User <rlarjsgl03@gmail.com>
2026-05-29 23:53:45 +0900

    feat: application connect

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,8 +1,8 @@
 class AppConstants {
   static const String appName = 'LearnPath AI';
 
-  // FastAPI 서버 주소
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  // FastAPI 서버 주소 ='http://127.0.1.1:8000'
+  static const String baseUrl = 'http://127.0.1.1:8000';
 
   static const List<String> difficulties = [
     '초급',
