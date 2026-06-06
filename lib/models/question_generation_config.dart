633a2d89a9a347997e1cde27415138ef41dbaa99
Kim GunHee <405zign@gmail.com>
2026-06-06 21:30:12 +0900

    Include generation mode and learning level in question config

--- lib/models/question_generation_config.dart
+++ lib/models/question_generation_config.dart
@@ -1,9 +1,12 @@
 import 'learning_mode.dart';
 import 'question_type.dart';
+import 'quiz_setup_options.dart';
 import 'subject_type.dart';
 
 class QuestionGenerationConfig {
   final LearningMode mode;
+  final QuestionGenerationMode generationMode;
+  final LearningLevel learningLevel;
   final SubjectType subjectType;
   final List<QuestionType> allowedTypes;
   final Map<QuestionType, int> weights;
@@ -12,6 +15,8 @@ class QuestionGenerationConfig {
 
   const QuestionGenerationConfig({
     required this.mode,
+    required this.generationMode,
+    required this.learningLevel,
     required this.subjectType,
     required this.allowedTypes,
     required this.weights,
@@ -22,6 +27,10 @@ class QuestionGenerationConfig {
   Map<String, dynamic> toJson() {
     return {
       'mode': mode.name,
+      'generation_mode': generationMode.apiValue,
+      'learning_level': learningLevel.apiValue,
+      'difficulty': learningLevel.label,
+      'use_uploaded_material': generationMode.requiresMaterial,
       'subject_type': subjectType.name,
       'allowed_question_types': allowedTypes.map((type) => type.apiValue).toList(),
       'question_type_weights': weights.map(
